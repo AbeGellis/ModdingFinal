ce3a9d54b6436650bc439acc6ca9de7329dd45a7
Abraham Gellis <aagellis@gmail.com>
2013-11-15 20:14:56 -0500

    Finished implementing simple AI: character moves to follow player.

--- Assets/Scripts/Brain/FollowBrain.cs
+++ Assets/Scripts/Brain/FollowBrain.cs
@@ -9,9 +9,29 @@ public class FollowBrain : Brain {
 	public float closeDistance; //Distance from the player where the character stops following
 	public float farDistance;	//Distance from the player where the character starts following
 	
+	bool following;	//Whether or not the character is following
+	
 	override public void Update () {
 		base.Update();
+		
+		//Vector describing difference between player/character positions
 		Vector3 toPlayer = CharacterControl.player.transform.position - body.transform.position;
+		
+		//Turn to face player
 		Look(toPlayer, turnSpeed);
+		
+		if (following) {	//Follows until too close
+			if (toPlayer.magnitude < closeDistance)
+				following = false;
+			else {
+				//Only follow if looking in player's general direction
+				if (Vector3.Angle(toPlayer, transform.forward) < 45f)	
+					body.Move(body.transform.forward);
+			}
+		}
+		else {	//Waits until too far
+			if (toPlayer.magnitude > farDistance)
+				following = true;
+		}
 	}
 }
