0df50c604f4608bc9fa91a932e1d9e79448dc433
Brian Kang <btk226@nyu.edu>
2013-11-23 19:40:37 -0500

    updated title screen with background and (not working) script that changes colors of text on mouseover.

--- Assets/Scripts/instructions.cs
+++ Assets/Scripts/instructions.cs
@@ -1,20 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * 11-22-13
+ * Change the color of the text on mouseover, click loads the instructions scene
+ * idea: have splahes of paint appear on the screen on mouseover (how to implement?)
+ * */
 public class instructions : MonoBehaviour {
-
+	
+	public TextMesh instruct;
+	Color newColor;
+	Color startColor;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
+		startColor = instruct.color;
+		newColor = Color.red;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if I detect the left-click then load the next level
-		//in this case, the next level is the scene that actually has the game in it
 		
-		if (Input.GetMouseButton (0) ) {
-			Application.LoadLevel (1);
+		//original color of text if not on mouseover
+		instruct.color = startColor;
+		
+		//construct ray, raycast
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rayHit = new RaycastHit();
+		
+		// if rayHit = sheep
+		if (Physics.Raycast (ray, out rayHit, 10000f))
+		{
+			//change the color of the text on mouseover
+			if (rayHit.collider.tag == "instructions")
+			{
+				instruct.color = newColor;
+				
+				//load instructions screen on click
+				if(Input.GetMouseButtonDown(0))
+				{
+					Application.LoadLevel (1);
+				}
+			}
+		
 		}
+		
 	}
 }
