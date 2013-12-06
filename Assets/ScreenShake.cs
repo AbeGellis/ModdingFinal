f8d0106476329659cfedc2c242367d7c470eb56d
Abraham Gellis <aagellis@gmail.com>
2013-12-06 17:01:26 -0500

    Started screen shake.  Edited level to set initial timer to non-zero (accidentally discarded that change)

---
+++ Assets/ScreenShake.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : MonoBehaviour {
+
+	static float shakeTimer;
+	public float shakeAmplitude = 1f;
+	public float shakeSlowTime = 1f;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (shakeTimer > 0f) {
+			shakeTimer -= Time.deltaTime;
+			transform.localPosition = new Vector3(Mathf.Sin(shakeTimer) * 
+                      shakeAmplitude * Mathf.Min(1f, shakeTimer/shakeSlowTime),
+		              transform.localPosition.y, transform.localPosition.z);
+		}
+		else {
+			shakeTimer = 0f;
+			transform.localPosition = new Vector3(0f,
+                      transform.localPosition.y, transform.localPosition.z);
+		}
+	}
+}
